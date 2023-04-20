9f3a2f92a82d6dd9bd0458f0f283444d1cb3a408
Allan <owinoallan77@gmail.com>
2023-04-20 12:16:34 +0300

    blogs

---
+++ home.php
@@ -0,0 +1,68 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>group5BlogPages</title>
+  <link rel="stylesheet" href="styles.css">
+  <link rel="icon" type="xtypes/images" href="./images/pexels-photo-533990.webp">
+</head>
+<body>
+    <nav>
+      <p>Wildlife</p>
+       <main id="main">
+        Terestrial
+        </main> 
+        
+        <div class="drop-down">
+          <h4 id="close">&times;</h4>
+          <h5>Plants</h5>
+          <h5>Aqua</h5>
+          <h5>Aves</h5>
+        </div>
+
+      <a href="#">Login</a>
+    </nav>
+    
+      <div class="main">
+        <section class="sec">
+          <div class="img">Image</div>
+          <p><span>c</span> This is a Rhino</p>
+          <a href="" target="blank">click to view more</a>
+        </section>
+        <section class="sec">
+          <div class="img">Image</div>
+          <p><span>c</span> This is a Rhino</p>
+          <a href="" target="blank">click to view more</a>
+        </section>
+        <section class="sec">
+          <div class="img">Image</div>
+          <p><span>c</span> This is a Rhino</p>
+          <a href="" target="blank">click to view more</a>
+        </section>
+        <section class="sec">
+          <div class="img">Image</div>
+          <p><span>c</span> This is a Rhino</p>
+          <a href="" target="blank">click to view more</a>
+        </section>
+        <section class="sec">
+          <div class="img">Image</div>
+          <p><span>c</span> This is a Rhino</p>
+          <a href="" target="blank">click to view more</a>
+        </section>
+        <section class="sec">
+          <div class="img">Image</div>
+          <p><span>c</span> This is a Rhino</p>
+          <a href="" target="blank">click to view more</a>
+        </section>
+        <section class="sec">
+          <div class="img">Image</div>
+          <p><span>c</span> This is a Rhino</p>
+          <a href="" target="blank">click to view more</a>
+        </section>
+      </div>
+  <script src="index.js"></script>
+  </body>
+</html>
---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+document.getElementById('main').addEventListener('click', ()=> {
+  document.querySelector('.drop-down').style.marginTop = "0";
+  document.querySelector('.drop-down').style.transition="all 0.3s ease-in-out";
+});
+
+const Close = document.getElementById('close');
+
+Close.addEventListener('click', () => {
+    document.querySelector('.drop-down').style.marginTop="-100%";
+    document.querySelector('.drop-down').style.transition="all 0.3s ease-in-out";
+})
